2aefa67bd76187829f21c400a1a20057ab85b553
Rbjett <Rbjett@gmail.com>
2024-09-21 21:13:26 +0930

    replaced db setup scripts

---
+++ scripts/sessions.sql
@@ -0,0 +1,19 @@
+USE `hiubwjjf_torque`;
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `v` varchar(1) NOT NULL,
+  `id` varchar(32) NOT NULL,
+  `session` varchar(15) NOT NULL,
+  `time` varchar(15) NOT NULL,
+  `eml` varchar(255) NOT NULL DEFAULT '0',
+  `profileName` varchar(255) NOT NULL DEFAULT 'Not Specified',
+  `profileFuelType` varchar(255) NOT NULL DEFAULT 'Not Specified',
+  `profileWeight` float NOT NULL DEFAULT 0,
+  `profileVe` float NOT NULL DEFAULT 0,
+  `profileFuelCost` float NOT NULL DEFAULT 0,
+  `timestart` varchar(15) NOT NULL DEFAULT '0',
+  `timeend` varchar(15) NOT NULL DEFAULT '0',
+  `sessionsize` varchar(15) NOT NULL DEFAULT '0',
+  UNIQUE KEY `session` (`session`,`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
